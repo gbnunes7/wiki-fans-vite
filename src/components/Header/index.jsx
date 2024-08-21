3ed6bd22a25af1a2597a48a5df0b573d1cd583c6
Gabriel Melo <bielrj170@gmail.com>
2024-08-20 23:46:49 -0300

    feat: add Header component with mobile-first styling

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header className="bg-primary-red min-h-16 px-12 py-4 flex"> 
+            <h1 className="text-3xl text-white">Wiki for fans</h1>
+            <button className="mx-4">Modal</button>
+        </header>
+    )
+}
+
+export default Header

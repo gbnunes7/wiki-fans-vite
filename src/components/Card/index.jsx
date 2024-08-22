598fad23c42d68d773e350b05df7f179d0d61e6b
Gabriel Melo <bielrj170@gmail.com>
2024-08-22 14:06:10 -0300

    style: add height and width for imgs

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,7 +1,7 @@
 const Card = ({src,alt,nome}) => {
     return (
             <div className="bg-light-red min-h-52 flex flex-col items-center p-2 rounded shadow-md shadow-black hover:cursor-pointer hover:scale-110 duration-100 ">
-                <img src={src} className="w-28 rounded m-2" alt={alt} />
+                <img src={src} className="w-28 h-44 rounded m-2 object-cover" alt={alt} />
                 <p>{nome}</p>
             </div>
     )

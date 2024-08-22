a8131e89b90b3d76308ea176ede0a97a4c415ae4
Gabriel Melo <bielrj170@gmail.com>
2024-08-22 14:10:20 -0300

    style: fix resolution of images and details characters

--- src/components/CharacterDetail/index.jsx
+++ src/components/CharacterDetail/index.jsx
@@ -22,8 +22,8 @@ const CharacterDetail = () => {
         <div className="h-screen bg-grey flex items-center justify-center"> 
             <div className="bg-white rounded-lg shadow-2xl max-w-4xl w-full flex flex-col pb-24 pt-12 px-4 mb-24"> 
                 <h2 className="text-3xl text-center mb-4">House of Dragons</h2> 
-                    <div className="mt-4 px-3.5 flex gap-8 justify-between mx-24 ">
-                        <img src={character.imagem} alt={character.alt} className="w-48 rounded m-2 shadow-2xl shadow-black" />
+                    <div className="mt-4 mr-3.5 flex gap-8 justify-between mx-24 ">
+                        <img src={character.imagem} alt={character.alt} className="w-52 h-80 rounded m-2 shadow-2xl shadow-black object-cover" />
                         <div className="mr-16 w-full">
                             <h1 className="mt-3 text-2xl">Name: {character.nome}</h1>
                             <p className="mt-3 text-2xl">Nickname: {character.nickname}  </p>

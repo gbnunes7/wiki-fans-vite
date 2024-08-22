8eb2cf9d8bc7afd1741f8cf85fd7cf4253f3b8c0
Gabriel Melo <bielrj170@gmail.com>
2024-08-22 06:40:22 -0300

    feat: add new roles in db

--- src/components/CharacterDetail/index.jsx
+++ src/components/CharacterDetail/index.jsx
@@ -22,14 +22,14 @@ const CharacterDetail = () => {
         <div className="h-screen bg-grey flex items-center justify-center"> 
             <div className="bg-white rounded-lg shadow-2xl max-w-4xl w-full flex flex-col pb-24 pt-12 px-4 mb-24"> 
                 <h2 className="text-3xl text-center mb-4">House of Dragons</h2> 
-                    <div className="mt-4 px-3.5 flex gap-8 justify-between flex-wrap mx-24">
+                    <div className="mt-4 px-3.5 flex gap-8 justify-between mx-24 ">
                         <img src={character.imagem} alt={character.alt} className="w-48 rounded m-2 shadow-2xl shadow-black" />
-                        <div className="mr-16">
+                        <div className="mr-16 w-full">
                             <h1 className="mt-3 text-2xl">Name: {character.nome}</h1>
-                            <p className="mt-3 text-2xl">Nickname:</p>
+                            <p className="mt-3 text-2xl">Nickname: {character.nickname}  </p>
                             <p className="mt-3 text-2xl">Dragon: {character.dragao}</p>
-                            <p className="mt-3 text-2xl">Age: </p>
-                            <p className="mt-3 text-2xl">Enemies: </p>
+                            <p className="mt-3 text-2xl">Age: {character.age}  </p>
+                            <p className="mt-3 text-2xl">Enemies: {character.enemies}  </p>
                         </div>
                     </div>
                 </div>

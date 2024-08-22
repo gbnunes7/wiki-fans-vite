f6ee8d0bc05407aa479306be97e1e6758f32cdc1
Gabriel Melo <bielrj170@gmail.com>
2024-08-22 01:45:15 -0300

    refact: refact layout main from mobile

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -23,8 +23,8 @@ const Home = () => {
         
         <div className="h-screen flex flex-col"> 
             <Header onClick={toggleModal}/>
-            <main className="bg-grey overflow-hidden h-full">
-                <div className="bg-white mx-auto flex h-full w-full max-w-5xl flex-col rounded-xl shadow-2xl my-10 max-h-[90%] relative py-4 items-center">
+            <main className="bg-grey md:overflow-hidden h-full">
+                <div className="bg-grey md:bg-white md:mx-auto flex md:h-full md:w-full md:max-w-5xl flex-col md:rounded-xl md:shadow-2xl my-10 md:max-h-[90%] md:relative md:py-4 items-center max-sm:gap-4">
                     <h2 className="text-2xl">House of Dragons</h2>
                     <ul className="mt-4 px-3.5 flex gap-8 justify-center flex-wrap">
                         {characters.map(character => <li key={character.id}> <Card src={character.imagem} alt={character.alt} nome={character.nome}/></li>)}

f9b85ebfbeabe76685d9e77e2ffd3e5afe11305e
Gabriel Melo <bielrj170@gmail.com>
2024-08-22 06:30:11 -0300

    style: style for card detail

--- src/components/CharacterDetail/index.jsx
+++ src/components/CharacterDetail/index.jsx
@@ -19,20 +19,21 @@ const CharacterDetail = () => {
     }, [id]);
 
     return (
-        <>
-        <div className="h-screen flex flex-col"> 
-            <main className="bg-grey md:overflow-hidden h-full">
-                <div className="bg-grey md:bg-white md:mx-auto flex md:h-full md:w-full md:max-w-5xl flex-col md:rounded-xl md:shadow-2xl my-10 md:max-h-[90%] md:relative md:py-4 items-center max-sm:gap-4">
-                    <h2 className="text-2xl">House of Dragons</h2>
-                    <div className="mt-4 px-3.5 flex gap-8 justify-center flex-wrap">
-                        <h1 className="text-3xl">{character.nome}</h1>
+        <div className="h-screen bg-grey flex items-center justify-center"> 
+            <div className="bg-white rounded-lg shadow-2xl max-w-4xl w-full flex flex-col pb-24 pt-12 px-4 mb-24"> 
+                <h2 className="text-3xl text-center mb-4">House of Dragons</h2> 
+                    <div className="mt-4 px-3.5 flex gap-8 justify-between flex-wrap mx-24">
                         <img src={character.imagem} alt={character.alt} className="w-48 rounded m-2" />
-                        <p className="mt-2">{character.dragao}</p>
+                        <div className="mr-16">
+                            <h1 className="mt-3 text-2xl">Name: {character.nome}</h1>
+                            <p className="mt-3 text-2xl">Nickname:</p>
+                            <p className="mt-3 text-2xl">Dragon: {character.dragao}</p>
+                            <p className="mt-3 text-2xl">Age: </p>
+                            <p className="mt-3 text-2xl">Enemies: </p>
+                        </div>
                     </div>
                 </div>
-            </main>
-        </div>
-        </>
+        </div> 
     )
 }
 
